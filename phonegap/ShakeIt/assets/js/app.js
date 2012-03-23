44cac44a15c9bb8d27ffb22b6eb2afd5193b4f42
ynonp <ynonperek@gmail.com>
2012-03-23 09:30:33 +0200

    fixed phonegap examples to match new build server

--- phonegap/ShakeIt/assets/js/app.js
+++ phonegap/ShakeIt/assets/js/app.js
@@ -15,15 +15,15 @@
                 change_x = Math.abs(that.old_x - acceleration.x),
                 change_y = Math.abs(that.old_y - acceleration.y),
                 change_z = Math.abs(that.old_z - acceleration.z);
-                
+
             if ( now - acceleration.timestamp > 100 ) {
                 return;
             }
-            
+
             if ( ( ! isNaN(change_x) ) && ( change_x > 2.25 ) ) {
                 shake = true;
             }
-           
+
             if ( ( ! isNaN(change_y) ) && ( change_y > 3 ) ) {
                 shake = true;
             }
@@ -31,38 +31,44 @@
             if ( ( ! isNaN(change_z) ) && ( change_z > 4.5 ) ) {
                 shake = true;
             }
-            
+
             if ( shake ) {
                 that.onShake();
                 that.stop();
                 setTimeout(that.start, 2000);
+            } else {
+                var dbg = document.querySelector('.debug');
+                var text = "change_x = " + change_x + ", change_y = " + change_y + ", change_z = " + change_z + "</br>";
+                dbg.innerHTML += text;
             }
-                        
+
             that.old_x = acceleration.x;
             // that.been_here = false;
-        }
-            
+        };
+
         var onError = function() {
             console.log('error getting accelerometer data');
-        }
-        
+        };
+
         /** PUBLIC FUNCTIONS **/
-        that.start = function() {            
+        that.start = function() {
             that.watch_id = navigator.accelerometer.watchAcceleration(
                                     onMotion, onError, { frequency : 100 });
             that.onShake = onShake;
-        }
-        
+        };
+
         that.stop = function() {
             navigator.accelerometer.clearWatch(that.watch_id);
-        }
-        
+        };
+
         return that;
-    }
-    
+    };
+
     function init() {
-        ShakeDetector(function() { document.getElementById('box').setAttribute('class', 'shaked') }).start();
+        ShakeDetector(function() {
+            document.getElementById('box').setAttribute('class', 'shaked'); 
+        }).start();
     }
-    
+
     document.addEventListener('deviceready', init, false);
 }(this));
